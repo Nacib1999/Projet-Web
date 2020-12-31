60239c2c89502f290e20767671b0918b98600518
souhail642 <chernisouhail642@gmail.com>
2020-12-31 17:20:17 +0100

    my 2 commit

--- Frontend/smartmed/team.php
+++ Frontend/smartmed/team.php
@@ -260,21 +260,25 @@ foreach($listePromotions as $row){
                             <h3 class="nameprod">Prix       : </h3> 
                             <h3 class="defprod"> <?PHP echo $row['prix']; ?> DT</h3>
                          </div>
+                         <div class="nt">  
+                             <a class='comment-reply-link nameprodd'> Noter:</a>
                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
   <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
 </svg> 
-                         <?php  echo "<a  class='comment-reply-link' href='faible.php?idproduit=" , $row['id'] , "'>  Faible </a>"; ?>
+                         <?php  echo "<a  class='comment-reply-link nameprodd'  href='faible.php?idproduit=" , $row['id'] , "'> Faible </a>"; ?>
                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart-half" viewBox="0 0 16 16">
   <path d="M8 2.748v11.047c3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
 </svg>
-                         <?php echo "<a  class='comment-reply-link' href='bien.php?idproduit=" , $row['id'] , "'>  Bien </a>"; ?>
+                         <?php echo "<a  class='comment-reply-link nameprodd'  href='bien.php?idproduit=" , $row['id'] , "'>  Moyen </a>"; ?>
 
 
                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
 </svg> 
 
-<?php echo "<a  class='comment-reply-link' href='excellent.php?idproduit=" , $row['id'] , "'>  Excellent </a>"; ?>
+<?php echo "<a  class='comment-reply-link nameprodd'  href='excellent.php?idproduit=" , $row['id'] , "'>  Excellent </a>"; ?>
+</div>
+                <div class="bt">
                  <?php if ( $row['note']>2){echo "Meilleur produit"; }
                   else 
                   if ( $row['note']>1){echo "Produit moyen"; }
@@ -283,11 +287,12 @@ foreach($listePromotions as $row){
                   ?>
                       </div>
                       </div>
-                      
+                     </div> 
                       <?PHP
 }
 ?>
- 
+                          <div class="rd">  
+
  <?php  
  $connect = mysqli_connect("localhost", "root", "", "projet");  
  $query = "SELECT categorie, count(*) as number FROM produit GROUP BY categorie";  
@@ -324,17 +329,17 @@ foreach($listePromotions as $row){
       </head>  
       <body>  
            <br /><br />  
-           <div style="width:900px;">  
+           <div style="width:500px;">  
                 <h3 align="center">ANALYSE STATISTIQUE</h3>  
                 <br />  
-                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+                <div id="piechart" style="width: 900px; height: 400px;"></div>  
            </div>  
       </body>  
  </html> 
  
 
 
-
+        </div>
 
 
 
